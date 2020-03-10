63e2db374d159c503671bdfa80ec3b229e5277f6
zeylin <ellinam31@gmail.com>
2020-03-10 19:14:25 +0600

    [Added] Initial commit [0.1.0]

---
+++ src/main/java/com/zeylin/eventsourcingwithcqrs/EventSourcingWithCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.zeylin.eventsourcingwithcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingWithCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingWithCqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeylin/eventsourcingwithcqrs/EventSourcingWithCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeylin.eventsourcingwithcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingWithCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
